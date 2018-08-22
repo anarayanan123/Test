1e7d8a61647b0bae8b9589f4219e149a60b916b9
Arun Narayanan <arunnarayanan@Aruns-MacBook-Pro.local>
2018-08-22 17:41:09 -0400

    pushing to heroku

--- applicationcopy.py
+++ applicationcopy.py
@@ -3,10 +3,11 @@
 #from cs50 import SQL
 import os
 import datetime 
+from flask_sqlalchemy import SQLAlchemy
 
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
+
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
