aeb3472fa4ca6c73d49ad0a4a409f13ab1490571
Arun Narayanan <arunnarayanan@Aruns-MacBook-Pro.local>
2018-08-22 14:49:34 -0400

    pushing to heroku

--- application copy.py
+++ application copy.py
@@ -1,17 +1,17 @@
 
 
-
-###removing SQL lite 
-#from cs50 import SQL
-import os
-from flask_sqlalchemy import SQLAlchemy 
-
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import apology, login_required, lookup, usd
+
+
+###removing SQL lite 
+#from cs50 import SQL
+import os
+from flask_sqlalchemy import SQLAlchemy 
 
 # Configure application
 app = Flask(__name__)
--- tables.py
+++ tables.py
@@ -1,7 +1,4 @@
-###removing SQL lite 
-#from cs50 import SQL
-import os
-from flask_sqlalchemy import SQLAlchemy 
+
 
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
@@ -10,6 +7,12 @@ from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import apology, login_required, lookup, usd
 
+
+###removing SQL lite 
+#from cs50 import SQL
+import os
+from flask_sqlalchemy import SQLAlchemy 
+
 # Configure application
 app = Flask(__name__)
 
@@ -17,18 +20,10 @@ app = Flask(__name__)
 ###if you do not have SQL Alchemy you will get this warning 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 ###config the DB to the environment DB URL
-app.config['SQLALCHEMY_TRACK_URI'] = os.environ['DATABASE_URL'] 
+app.config['SQLALCHEMY_TRACK_URI'] = 'os.environ['DATABASE_URL'] '
 ###config
 db = SQLAlchemy(app)
 class Users(db.Model):
+    __tablename__ = "Users"
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(80), unique = True)
-    hash = db.Column(db.String(80))
-    cash = db.Column(db.Float, default = 10000.00)
-class Portfolio(db.Model):
-    pid = db.Column(db.Integer, primary_key = True)
-    TransDate = db.Column(db.DateTime)
-    User = db.Column(db.String(80))
-    Stock = db.Column(db.String(80))
-    Price = db.Column(db.Float)
-    Num = db.Column(db.Integer)
