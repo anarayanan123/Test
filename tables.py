148f52c8908587469ab61d052027b63c6d0f6ea5
Arun Narayanan <arunnarayanan@Aruns-MacBook-Pro.local>
2018-08-22 14:52:57 -0400

    pushing to heroku

--- tables.py
+++ tables.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 ###if you do not have SQL Alchemy you will get this warning 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 ###config the DB to the environment DB URL
-app.config['SQLALCHEMY_TRACK_URI'] = 'os.environ['DATABASE_URL'] '
+app.config['SQLALCHEMY_TRACK_URI'] = 'postgres://okvpxcvauaofmd:a57bf0462d83f843211915ad4c7b043a7883d1ef988b31c502860dac75d4406d@ec2-54-163-246-5.compute-1.amazonaws.com:5432/ddplart5et8pfb'
 ###config
 db = SQLAlchemy(app)
 class Users(db.Model):
