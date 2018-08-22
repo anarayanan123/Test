1790b0fbc8ac3b7df1ac590d16435649c1150895
Arun Narayanan <arunnarayanan@Aruns-MacBook-Pro.local>
2018-08-22 18:56:43 -0400

    pushing to heroku

--- model1.py
+++ model1.py
@@ -6,11 +6,11 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_TRACK_URI']='postgres://okvpxcvauaofmd:a57bf0462d83f843211915ad4c7b043a7883d1ef988b31c502860dac75d4406d@ec2-54-163-246-5.compute-1.amazonaws.com:5432/ddplart5et8pfb'
+app.config['SQLALCHEMY_TRACK_URI']='postgres://hvfoqbqasxxmco:6c30c1c60c445054f9b1afdf9613fddf06331927a4a9c526c63d60910dc93f2c@ec2-54-83-3-101.compute-1.amazonaws.com:5432/dbaeatcsu7fc1p'
 
 ###config
-db = SQLAlchemy(app)
-
+db = SQLAlchemy()
+db.init_app(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
